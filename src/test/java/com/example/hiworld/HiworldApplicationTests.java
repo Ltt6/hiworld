11f56f28bc6ad8396821a1e1b3d05a34b6d7fd4e
Ltt <2893603589@qq.com>
2022-05-15 12:06:31 +0800

    test

---
+++ src/test/java/com/example/hiworld/HiworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
